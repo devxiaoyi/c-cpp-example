b38f028608417a4531c15c95493732b6207b5ec4
Don <devxiaoyi@gmail.com>
2022-11-26 14:37:57 +0800

    add file

---
+++ sleep_for_fps.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int main() {
+	uint64_t m_PreviousVsync = 0;
+    int32_t targetFrameRate = 30; // target to 30 fps
+
+	while (1) {
+		uint64_t current = std::chrono::duration_cast<std::chrono::microseconds>(
+                std::chrono::system_clock::now().time_since_epoch()).count();
+		uint64_t interval = 1000 * 1000 / targetFrameRate;
+
+		if (m_PreviousVsync + interval > current) {
+			uint64_t sleepTimeMs = (m_PreviousVsync + interval - current) / 1000;
+
+			if (sleepTimeMs > 0) {
+				// printf("Sleep %llu ms for next VSync.\n", sleepTimeMs);
+				std::this_thread::sleep_for(std::chrono::milliseconds(sleepTimeMs));
+			}
+
+			m_PreviousVsync += interval;
+		}
+		else {
+			m_PreviousVsync = current;
+		}
+        printf("add some function here\n");
+	}
+
+    return 0;
+}
